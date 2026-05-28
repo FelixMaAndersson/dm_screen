953c21d981c439a27bd9d67b4f95eca48570339a
Felix Andersson <felix.andersson@gmail.com>
2026-05-28 13:49:53 +0200

    set up basic interfaces in dataacesss

--- src/main/java/se/yrgo/dataaccess/UserDao.java
+++ src/main/java/se/yrgo/dataaccess/UserDao.java
@@ -6,5 +6,9 @@ import se.yrgo.domain.User;
 import se.yrgo.exceptions.UserNotFoundException;
 
 public interface UserDao {
-
+public void createUser (User user);
+public void updateUser (User user);
+public void deleteUser (User user);
+public User getUserById (int id);
+public User getUserByName (String name);
 }
