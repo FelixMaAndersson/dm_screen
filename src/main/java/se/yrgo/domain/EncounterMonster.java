74763e0eb38bd1a2b670ea62d6c6d9663cc1b193
Felix Andersson <felix.andersson@gmail.com>
2026-06-02 13:23:15 +0200

    generated constructor, getters and setters

--- src/main/java/se/yrgo/domain/EncounterMonster.java
+++ src/main/java/se/yrgo/domain/EncounterMonster.java
@@ -22,6 +22,63 @@ public class EncounterMonster {
     private boolean bbeg;
     private String lore;
 
+    public EncounterMonster(Monster monster, Encounter encounter, int currentHp, boolean alive, boolean bbeg, String lore) {
+        this.monster = monster;
+        this.encounter = encounter;
+        this.currentHp = currentHp;
+        this.alive = alive;
+        this.bbeg = bbeg;
+        this.lore = lore;
+    }
+
+    public EncounterMonster() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Encounter getEncounter() {
+        return encounter;
+    }
+
+    public void setEncounter(Encounter encounter) {
+        this.encounter = encounter;
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
+    }
+
+    public void setCurrentHp(int currentHp) {
+        this.currentHp = currentHp;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public boolean isBbeg() {
+        return bbeg;
+    }
+
+    public void setBbeg(boolean bbeg) {
+        this.bbeg = bbeg;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
+
     public Monster getMonster() {
         return monster;
     }
