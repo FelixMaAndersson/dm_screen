e715b17d79c3af386edb2a5df1ece8af0ec231f0
Felix Andersson <felix.andersson@gmail.com>
2026-06-03 19:50:45 +0200

    added create logic in encounter service

--- src/main/java/se/yrgo/services/encounter/EncounterService.java
+++ src/main/java/se/yrgo/services/encounter/EncounterService.java
@@ -3,21 +3,40 @@ package se.yrgo.services.encounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import se.yrgo.dataaccess.CampaignRepository;
 import se.yrgo.dataaccess.EncounterRepository;
+import se.yrgo.domain.Campaign;
 import se.yrgo.domain.Encounter;
+import se.yrgo.dto.encounter.CreateEncounterRequest;
+import se.yrgo.exceptions.CampaignNotFoundException;
 
 @Service
 @Transactional
 public class EncounterService {
 
     private final EncounterRepository repository;
+    private final CampaignRepository campaignRepository;
 
     @Autowired
-    public EncounterService(EncounterRepository repository) {
+    public EncounterService(EncounterRepository repository, CampaignRepository campaignRepository) {
         this.repository = repository;
+        this.campaignRepository = campaignRepository;
     }
 
-    public Encounter createEncounter() {
+    // CREATE
 
+    public Encounter createEncounter(CreateEncounterRequest request) throws CampaignNotFoundException {
+        Campaign campaign = campaignRepository.findById(request.campaignId())
+                .orElseThrow(() -> new CampaignNotFoundException(request.campaignId()));
+
+        Encounter e = new Encounter(
+                request.habitat(),
+                request.visionDistance(),
+                campaign
+        );
+
+        return repository.save(e);
     }
+
+
 }
