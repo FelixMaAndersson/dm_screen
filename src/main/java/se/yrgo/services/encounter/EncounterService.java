a382438e2a8bb06a2486413e252cb2abc2e80399
Felix Andersson <felix.andersson@gmail.com>
2026-06-03 20:24:28 +0200

    added response help method and read operations in encounter service

--- src/main/java/se/yrgo/services/encounter/EncounterService.java
+++ src/main/java/se/yrgo/services/encounter/EncounterService.java
@@ -7,8 +7,15 @@ import se.yrgo.dataaccess.CampaignRepository;
 import se.yrgo.dataaccess.EncounterRepository;
 import se.yrgo.domain.Campaign;
 import se.yrgo.domain.Encounter;
+import se.yrgo.domain.EncounterMonster;
+import se.yrgo.domain.Monster;
 import se.yrgo.dto.encounter.CreateEncounterRequest;
+import se.yrgo.dto.encounter.EncounterResponse;
+import se.yrgo.dto.encounterMonster.EncounterMonsterSummary;
 import se.yrgo.exceptions.CampaignNotFoundException;
+import se.yrgo.exceptions.EncounterNotFoundException;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -38,5 +45,51 @@ public class EncounterService {
         return repository.save(e);
     }
 
+    // READ
+
+    private Encounter getEncounterById(Long id) {
+        return getOrThrow(id);
+    }
+
+    public List<Encounter> getAllEncounters() {
+        return repository.findAll();
+    }
+
+    public List<EncounterResponse> getAllEncountersInCampaign(Long campaignId) {
+        return repository.findByCampaignId(campaignId)
+                .stream()
+                .map(this::toResponse)
+                .toList();
+    }
+
+    // HELP METHODS
+
+    private Encounter getOrThrow(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EncounterNotFoundException(id));
+    }
+
+    private EncounterResponse toResponse(Encounter e) {
+        List<EncounterMonsterSummary> monsters =
+                e.getEncounterMonsters()
+                        .stream()
+                        .map(em -> new EncounterMonsterSummary(
+                                em.getId(),
+                                em.getMonster().getId(),
+                                em.getMonster().getName()
+                        ))
+                        .toList();
+
+        return new EncounterResponse(
+                e.getId(),
+                e.getHabitat(),
+                e.getVisionDistance(),
+                e.getCampaign().getId(),
+                e.getCampaign().getName(),
+                monsters,
+                e.getEncounterDifficulty(),
+                e.getLore()
+        );
+    }
 
 }
