b365b3b8504d1a73964eef2d1eeb4b02dd7541c8
Felix Andersson <felix.andersson@gmail.com>
2026-06-03 19:11:16 +0200

    created exception for encounters

--- src/main/java/se/yrgo/exceptions/EncounterMonsterNotFoundException.java
+++ src/main/java/se/yrgo/exceptions/EncounterMonsterNotFoundException.java
@@ -5,8 +5,4 @@ public class EncounterMonsterNotFoundException extends RuntimeException {
     public EncounterMonsterNotFoundException(long id) {
         super("Encounter monster not found with id: " + id);
     }
-
-    public EncounterMonsterNotFoundException(String name) {
-        super("Monster not found with name: " + name);
-    }
 }
