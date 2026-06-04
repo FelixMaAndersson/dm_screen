c4c25e179037e83589bd5243a708478f2ea3d8c4
Felix Andersson <felix.andersson@gmail.com>
2026-06-04 15:28:02 +0200

    lazy fixed cross-origin problem

--- backend/src/main/java/se/yrgo/controllers/MonsterController.java
+++ backend/src/main/java/se/yrgo/controllers/MonsterController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/monsters")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MonsterController {
 
     private final MonsterService service;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import CampaignList from "./components/CampaignList"
 import CreateUserForm from "./components/CreateUserForm";
+import CreateCampaignForm from "./components/CreateCampaignForm.tsx";
 
 function App() {
     return (
         <div>
             <h1>D&D Screen</h1>
             <CreateUserForm />
+            <CreateCampaignForm />
             <CampaignList />
         </div>
     )
